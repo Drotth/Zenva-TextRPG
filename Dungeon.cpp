e3c8bcf07e6b430704e8e7a0875af3f5d7c2cd83
Andreas Drotth <drotth@hotmail.com>
2021-09-23 15:45:01 +0200

    Added fighting and movement to Dungeon class

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -18,6 +18,7 @@ int Dungeon::runDungeon()
     while (true)
     {
         enterRoom(player.currentRoom);
+        handleMovementActions(player.currentRoom);
     }
 }
 
@@ -57,7 +58,7 @@ void Dungeon::handleEmptyRoom(Room *room)
     }
 }
 
-void Dungeon::handleRoomWithChest(Room * room)
+void Dungeon::handleRoomWithChest(Room *room)
 {
     cout << "You enter the room and see a large chest in the middle.\n";
     string actions[] = {
@@ -84,30 +85,192 @@ void Dungeon::handleRoomWithChest(Room * room)
     }
 }
 
-void Dungeon::handleRoomWithEnemy(Room * room)
+void Dungeon::handleRoomWithEnemy(Room *room)
 {
+    GameCharacter enemy = room->enemies.front();
+    cout << "You enter the room and see a " << enemy.name << ".\n";
+    string actions[] = {
+        "a. Fight the " + enemy.name,
+        "b. Go back to previous room"};
+
+    while (true)
+    {
+        printActions(2, actions);
+        string input;
+        cin >> input;
+        if (input == "a")
+        {
+            handleFightActions(&enemy);
+            return;
+        }
+        else if (input == "b")
+        {
+            player.changeRooms(player.previousRoom);
+            enterRoom(player.currentRoom);
+            return;
+        }
+        {
+            cout << "Incorrect choice.\n";
+        }
+    }
 }
 
-void Dungeon::handleLootActions(Room * room)
+void Dungeon::handleLootActions(Room *room)
 {
     Item item = room->items.front();
     int size = room->items.size();
     player.lootRoom(room);
     room->clearLoot();
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++)
+    {
         cout << "You open the chest and find a " << item.name << ".\n";
-        cout << "Your health is now" << player.currentHealth 
-        << ", your attack is now " << player.attack 
-        << ", and your defence is now " << player.defence << ".\n";
+        cout << "Your health is now" << player.currentHealth
+             << ", your attack is now " << player.attack
+             << ", and your defence is now " << player.defence << ".\n";
     }
 }
 
-void Dungeon::handleFightActions(Room * room)
+void Dungeon::handleFightActions(GameCharacter *enemy)
 {
+    //cout << "You enter the room and see a " << enemy.name << ".\n";
+    string actions[] = {
+        "a. Attack",
+        "b. Retreat"};
+
+    while (true)
+    {
+        printActions(2, actions);
+        string input;
+        cin >> input;
+
+        //Handle player actions
+        if (input == "a")
+        {
+            int damage = enemy->takeDamage(player.attack);
+            cout << "Your attack does " << damage << " damage.\n";
+        }
+        else if (input == "b")
+        {
+            player.changeRooms(player.previousRoom);
+            enterRoom(player.currentRoom);
+            return;
+        }
+        {
+            cout << "Incorrect choice.\n";
+        }
+
+        //Check if enemy is dead
+        if (enemy->checkIsDead())
+        {
+            cout << "you win! You have defeated the " << enemy->name << ".\n";
+            player.increaseStats(10, 5, 5);
+            cout << "Your health is now" << player.currentHealth
+                 << ", your attack is now " << player.attack
+                 << ", and your defence is now " << player.defence << ".\n";
+            player.currentRoom->clearEnemies();
+            return;
+        }
+
+        //Handle enemy actions
+        int damage = player.takeDamage(enemy->attack);
+        cout << enemy->name << "'s attack does' " << damage << " damage.\n";
+        cout << "You now have " << player.currentHealth << " health.\n";
+        if (player.checkIsDead())
+        {
+            return;
+        }
+    }
 }
 
-void Dungeon::handleMovementActions(Room * room)
+void Dungeon::handleMovementActions(Room *room)
 {
+    while (true)
+    {
+        if (room->pos == 0)
+        {
+            string actions[] = {
+                "a. Move right",
+                "b. Move down"};
+            printActions(2, actions);
+            string input;
+            cin >> input;
+
+            if (input == "a")
+            {
+                player.changeRooms(&rooms[1]);
+                return;
+            }
+            else if (input == "b")
+            {
+                player.changeRooms(&rooms[2]);
+                return;
+            }
+            else
+            {
+                cout << "Incorrect choice.\n";
+            }
+        }
+        else if (room->pos == 1)
+        {
+            string actions[] = {
+                "a. Move left"};
+            printActions(1, actions);
+            string input;
+            cin >> input;
+
+            if (input == "a")
+            {
+                player.changeRooms(&rooms[0]);
+                return;
+            }
+            else
+            {
+                cout << "Incorrect choice.\n";
+            }
+        }
+        else if (room->pos == 2)
+        {
+            string actions[] = {
+                "a. Move up",
+                "b. Move right"};
+            printActions(2, actions);
+            string input;
+            cin >> input;
+
+            if (input == "a")
+            {
+                player.changeRooms(&rooms[0]);
+                return;
+            }
+            else if (input == "b")
+            {
+                player.changeRooms(&rooms[3]);
+                return;
+            }
+            else
+            {
+                cout << "Incorrect choice.\n";
+            }
+        }
+        else
+        {
+            string actions[] = {
+                "a. Move left"};
+            printActions(1, actions);
+            string input;
+            cin >> input;
+
+            if (input == "a")
+            {
+                player.changeRooms(&rooms[2]);
+                return;
+            }
+            else
+            {
+                cout << "Incorrect choice.\n";
+            }
+        }
+    }
 }
 
 void Dungeon::printActions(int numActions, string actions[])
